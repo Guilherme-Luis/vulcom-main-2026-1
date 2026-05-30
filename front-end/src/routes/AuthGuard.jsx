6ae643d19c8a0d9a95af4632e051f9a5f47b9bb8
GuilhermeLuis242 <guilherme.rodrigues@ecenture.com.br>
2026-05-30 10:26:36 -0300

    (30/05) Reorganização e segurança de rotas no front-end

--- front-end/src/routes/AuthGuard.jsx
+++ front-end/src/routes/AuthGuard.jsx
@@ -6,7 +6,10 @@ import useWaiting from '../ui/useWaiting'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
-export default function AuthGuard({ children, adminOnly = false }) {
+
+import { UserLevel } from './routes'
+
+export default function AuthGuard({ children, userLevel = UserLevel.ANY }) {
 
   const { setAuthUser, authUser, setRedirectLocation } = React.useContext(AuthUserContext)
   const [status, setStatus] = React.useState('IDLE')
@@ -16,13 +19,13 @@ export default function AuthGuard({ children, adminOnly = false }) {
   const navigate = useNavigate()
 
   async function checkAuthUser() {
-    if(setStatus) setStatus('PROCESSING')
+    if (setStatus) setStatus('PROCESSING')
     showWaiting(true)
     try {
       const authUser = await myfetch.get('/users/me')
       setAuthUser(authUser)
     }
-    catch(error) {
+    catch (error) {
       setAuthUser(null)
       console.error(error)
       navigate('/login', { replace: true })
@@ -36,26 +39,41 @@ export default function AuthGuard({ children, adminOnly = false }) {
   React.useEffect(() => {
     // Salva a rota atual para posterior redirecionamento,
     // caso a rota atual não seja o próprio login
-    if(! location.pathname.includes('login')) setRedirectLocation(location)
+    if (!location.pathname.includes('login')) setRedirectLocation(location)
 
     checkAuthUser()
   }, [location])
 
   // Enquanto ainda não temos a resposta do back-end para /users/me,
   // exibimos um componente Waiting
-  if(status === 'PROCESSING') return <Waiting />
+  if (status === 'PROCESSING') return <Waiting />
 
-  if(authUser) {
-    if(adminOnly && authUser.is_admin) return children
-    else if (adminOnly && !(authUser.is_admin)) return (
-      <Box>
-        <Typography variant="h2" color="error">
-          Acesso negado
-        </Typography>
-      </Box>
-    )
-    else return children
+  /*
+    Se não há usuário autenticado e o nível de acesso assim o
+    exige, redirecionamos para a página de login
+  */
+  if (!authUser && userLevel > UserLevel.ANY) {
+    console.log({ authUser, userLevel })
+    return <Navigate to="/login" replace />
   }
-  else return <Navigate to="/login" replace />
-  
+
+  /*
+    Senão, se há um usuário não administrador tentando acessar uma
+    rota exclusiva para esse nível, mostramos uma mensagem de acesso negado
+  */
+  if (!(authUser?.is_admin) && userLevel === UserLevel.ADMIN) return (
+    <Box>
+      <Typography variant="h2" color="error">
+        Acesso negado
+      </Typography>
+    </Box>
+  )
+
+  /*
+    Se chegou até aqui, é porque a rota é liberada para qualquer
+    um ou o usuário possui autorização para acessar o
+    nível
+  */
+  console.log('AUTHGUARD:', authUser)
+  return children
 }
